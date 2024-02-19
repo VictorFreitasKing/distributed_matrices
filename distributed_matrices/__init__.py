58cb3b445c087781add996b369b88c48f85345b6
Victor Freitas <victorfreitasking@gmail.com>
2024-02-19 10:07:46 -0300

    resources commit

---
+++ distributed_matrices/__init__.py
@@ -0,0 +1,17 @@
+# inicia programa
+
+# Ler parâmetros de entrada no Arquivo
+
+# Carrega arquivo com a matriz a ser multiplicada
+
+# Inicia Contagem de TEMPO
+
+# Chama função de multiplicação (remota e ou local)
+
+# Termina Multiplicação (retorna todos os resultados)
+
+# Agrupa os resultados
+
+# Termina Contagem de TEMPO
+
+# Escreve arquivo de saída.
