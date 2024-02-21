0a6f719e1e99baa8e7e95dded34fa7c14aba2a93
Victor Freitas <victorfreitasking@gmail.com>
2024-02-21 20:59:18 -0300

    resources commit

--- distributed_matrices/__init__.py
+++ distributed_matrices/__init__.py
@@ -3,5 +3,5 @@ UFRPE - Universidade Federal Rural de Pernambuco
 UAST - Unidade Acadêmicas de Serra Talhada
 
 Alunos: José Victor de Freitas Nunes
-        Julio Costa
+        Julio Cesar da Costa Lopes
 '''
